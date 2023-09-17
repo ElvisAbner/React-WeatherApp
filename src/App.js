e96960e109f756daa9e5ed071ab9a1768a1edbf5
Elvis Estevez <124850509+ElvisAbner@users.noreply.github.com>
2023-09-17 11:18:32 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Search from "./components/search/search";
 import CurrentWeather from "./components/current-weather/current-weather";
 import Forecast from "./components/forecast/forecast";
@@ -8,6 +8,33 @@ import "./App.css";
 function App() {
   const [currentWeather, setCurrentWeather] = useState(null);
   const [forecast, setForecast] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchWeatherData = async () => {
+      const [lat, lon] = navigator.geolocation.getCurrentPosition().coords;
+
+      const currentWeatherFetch = fetch(
+        `${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`
+      );
+      const forecastFetch = fetch(
+        `${WEATHER_API_URL}/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`
+      );
+
+      Promise.all([currentWeatherFetch, forecastFetch])
+        .then(async (response) => {
+          const weatherResponse = await response[0].json();
+          const forcastResponse = await response[1].json();
+
+          setCurrentWeather({ city: "Your current location", ...weatherResponse });
+          setForecast({ city: "Your current location", ...forcastResponse });
+          setLoading(false);
+        })
+        .catch(console.log);
+    };
+
+    fetchWeatherData();
+  }, []);
 
   const handleOnSearchChange = (searchData) => {
     const [lat, lon] = searchData.value.split(" ");
@@ -33,8 +60,9 @@ function App() {
   return (
     <div className="container">
       <Search onSearchChange={handleOnSearchChange} />
-      {currentWeather && <CurrentWeather data={currentWeather} />}
-      {forecast && <Forecast data={forecast} />}
+      {loading && <div>Loading...</div>}
+      {currentWeather && !loading && <CurrentWeather data={currentWeather} />}
+      {forecast && !loading && <Forecast data={forecast} />}
     </div>
   );
 }
